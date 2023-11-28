7cf02332827e37e60bb2b79a68db7703475cd93f
Pavlo Nahurnyi <polusnp@gmail.com>
2023-11-29 01:30:17 +0200

    added functions

--- src/App.js
+++ src/App.js
@@ -9,6 +9,97 @@ function App() {
   const [output, setOutput] = useState("")
   const [calculatorData, setCalculatorData] = useState("")
 
+  const handleSubmit = () => {
+    const total = eval(calculatorData)
+    setInput(`${total}`)
+    setOutput(`${total}`)
+    setCalculatorData(`${total}`)
+  }
+
+  const handleClear = () => {
+    setInput("0")
+    setOutput("")
+    setCalculatorData("")
+  }
+
+  const handleNumbers = (value) => {
+    if (!calculatorData.length) {
+      setInput(`${value}`)
+      setCalculatorData(`${value}`)
+    } else {
+      if (value === 0 && (calculatorData === "0" || input === "0")) {
+        setCalculatorData(`${calculatorData}`)
+      } else {
+        const lastChat = calculatorData.charAt(calculatorData.length - 1)
+        const isLastChatOperator =
+          lastChat === "*" || operators.includes(lastChat)
+
+        setInput(isLastChatOperator ? `${value}` : `${input}${value}`)
+        setCalculatorData(`${calculatorData}${value}`)
+      }
+    }
+  }
+
+  const dotOperator = () => {
+    const lastChat = calculatorData.charAt(calculatorData.length - 1)
+    if (!calculatorData.length) {
+      setInput("0.")
+      setCalculatorData("0.")
+    } else {
+      if (lastChat === "*" || operators.includes(lastChat)) {
+        setInput("0.")
+        setCalculatorData(`${calculatorData} 0.`)
+      } else {
+        setInput(
+          lastChat === "." || input.includes(".") ? `${input}` : `${input}.`
+        )
+        const formattedValue =
+          lastChat === "." || input.includes(".")
+            ? `${calculatorData}`
+            : `${calculatorData}.`
+        setCalculatorData(formattedValue)
+      }
+    }
+  }
+
+  const handleOperators = (value) => {
+    if (calculatorData.length) {
+      setInput(`${value}`)
+      const beforeLastChat = calculatorData.charAt(calculatorData.length - 2)
+
+      const beforeLastChatIsOperator =
+        operators.includes(beforeLastChat) || beforeLastChat === "*"
+
+      const lastChat = calculatorData.charAt(calculatorData.length - 1)
+
+      const lastChatIsOperator =
+        operators.includes(lastChat) || lastChat === "*"
+
+      const validOp = value === "x" ? "*" : value
+      if (
+        (lastChatIsOperator && value !== "-") ||
+        (beforeLastChatIsOperator && lastChatIsOperator)
+      ) {
+        if (beforeLastChatIsOperator) {
+          const updatedValue = `${calculatorData.substring(
+            0,
+            calculatorData.length - 2
+          )}${value}`
+          setCalculatorData(updatedValue)
+        } else {
+          setCalculatorData(
+            `${calculatorData.substring(
+              0,
+              calculatorData.length - 1
+            )}${validOp}`
+          )
+        }
+      } else {
+        setCalculatorData(`${calculatorData}${validOp}`)
+      }
+    }
+  }
+
   const handleInput = (value) => {
     const number = numbers.find((num) => num === value)
     const operator = operators.find((oper) => oper === value)
